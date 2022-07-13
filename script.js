7cf0211c283eb81ff2db3a1af2871ff5354b3356
DT0998 <duongkytrieuvn@gmail.com>
2022-07-13 15:59:59 +0700

    update top level await

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
 // import modules
-import './shoppingCart'
-console.log("importing modules");
+// import {addToCart,totalPrice as price,tq} from './shoppingCart.js';
+// addToCart('Bread', 5);
+// console.log(price,tq);
+console.log('importing modules');
+
+// import * as ShoppingCart from './shoppingCart.js';
+// ShoppingCart.addToCart('Bread', 5);
+// console.log(ShoppingCart.totalPrice);
+import add, { cart } from './shoppingCart.js';
+add('pizza', 5);
+add('Bread', 5);
+add('apple', 5);
+
+console.log(cart);
+
+const getLastPost = async function () {
+  const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+  const data = await res.json();
+  console.log(data);
+  return { title: data.at(-1).title, text: data.at(-1).body };
+};
+const lastPost = getLastPost();
+console.log(lastPost);
+
+const lastPost2 = await getLastPost();
+console.log(lastPost2);
--- shoppingCart.js
+++ shoppingCart.js
@@ -1,1 +1,19 @@
 // export module
+console.log('Exporting module');
+// block of code
+console.log('start fetching data');
+await fetch('https://jsonplaceholder.typicode.com/users');
+console.log("finished fetching data");
+const shippingCost = 10;
+export const cart = [];
+export const addToCart = (product, quantity) => {
+  cart.push({ product, quantity });
+  console.log(`${quantity} ${product} added to cart`);
+};
+const totalPrice = 237;
+const totalQuantity = 23;
+export { totalPrice, totalQuantity as tq };
+export default (product, quantity) => {
+  cart.push({ product, quantity });
+  console.log(`${quantity} ${product} added to cart`);
+};
